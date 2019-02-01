851642786eb4c0b4f4326841d8a20047f90ecf97
lsy563193 <lsy563193@163.com>
2019-02-01 13:04:25 +0800

    add node

---
+++ src/path_plan_node.cpp
@@ -0,0 +1,17 @@
+//
+// Created by syue on 19-2-1.
+//
+
+//#include <move_base/move_base.h>
+
+int main(int argc, char** argv){
+//  ros::init(argc, argv, "move_base_node");
+//  tf::TransformListener tf(ros::Duration(10));
+//
+//  move_base::MoveBase move_base( tf );
+//
+//  //ros::MultiThreadedSpinner s;
+//  ros::spin();
+
+  return(0);
+}
