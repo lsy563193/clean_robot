a74a159ba8d5058d4739970e173c6e9d2607fe59
lsy563193 <lsy563193@163.com>
2019-02-01 17:37:35 +0800

    add follow wall mode

--- src/path_plan_node.cpp
+++ src/path_plan_node.cpp
@@ -4,25 +4,10 @@
 
 #include "path_plan/robot.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-
-
 int main(int argc, char** argv){
 
   ros::init(argc, argv, "path_plan_node");
-//  tf::TransformListener tf(ros::Duration(10));
-//
-//  robot::Robot robot(/*tf*/);
-//  ros::NodeHandle n("~");
-//  n.subscribe("normal_clean", 1, &Robot::KeyCb, this);
-
   Robot robot;
-//
-
-//  //ros::MultiThreadedSpinner s;
   ros::spin();
 
   return(0);
